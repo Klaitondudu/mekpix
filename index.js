b86db8203c9ade6199e56f9dbdca7dc2d857e387
klaiton <klaiton.almeida@ibstecnologia.com.br>
2023-02-25 21:47:27 -0300

    em processo de login e vercel

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const session =  require('express-session')
+
+
 const app = express()
 const path = require('path')
 const router = express.Router()
 var favicon = require('serve-favicon');
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 router.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname+'/src/index.html'))
 })
+
+app.get('/login', (req, res) => {
+	res.sendFile(__dirname + '/src/login.html');
+  });
 
 
 app.use('/', router)
